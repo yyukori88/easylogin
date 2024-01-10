0e3d072b2b755787138089842495071c87770845
yyukori88 <yyukori88@gmail.com>
2024-01-10 22:23:38 +0900

    easylogin

---
+++ src/main/java/jp/co/internous/easylogin/model/mappar/MstUserMapper.java
@@ -0,0 +1,13 @@
+package jp.co.internous.easylogin.model.mappar;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import jp.co.internous.easylogin.model.domain.MstUser;
+
+@Mapper
+public interface MstUserMapper {
+
+	@Select("SELECT * FROM mst_user WHERE user_name = #{userName} AND password =#{password}")
+	MstUser findByUserNameAndPassword(String userName,String password);
+}
